e02810c947abc241c732d5b73d505fb5687aa684
Fabho <fabian.calsina@gmail.com>
2018-11-11 23:34:42 -0400

    redux form

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,13 +1,72 @@
 import React, { Component } from 'react';
+import { Field, reduxForm } from 'redux-form';
 
 class PostsNew extends Component {
-  render () {
+
+  renderField (field) {
     return (
-      <div>
-        New Posts!
+      <div className="form-group has-danger">
+        <label>{field.label}</label>
+        <input
+          type="text"
+          className="form-control"
+          {...field.input}
+        />
+        <div className="text-help">
+          {field.meta.touched ? field.meta.error : ''}
+        </div>
       </div>
+    );
+  }
+
+  onSubmit(values) {
+    console.log(values);
+  }
+
+  render () {
+    const {handleSubmit} = this.props;
+    return (
+      <form onSubmit={this.props.handleSubmit(this.onSubmit.bind(this))}>
+        <Field
+          label="Title"
+          name="title"
+          component={this.renderField}
+        />
+        <Field
+          label="Tags"
+          name="categories"
+          component={this.renderField}
+        />
+        <Field
+          label="Post Content"
+          name="content"
+          component={this.renderField}
+        />
+        <button type="submit" className="btn btn-primary">Submit</button>
+      </form>
     );
   }
 }
 
-export default PostsNew;
+function validate(values) {
+  const errors = {};
+
+  if(!values.title || values.title.length < 3) {
+    errors.title = 'Enter a title!';
+  }
+
+  if(!values.categories) {
+    errors.categories = 'Enter categories!';
+  }
+
+  if(!values.content) {
+    errors.content = 'Enter some content pls!';
+  }
+
+  return errors;
+}
+
+export default reduxForm({
+  validate,
+  form: 'PostsNewForm'
+})(PostsNew);
